f17ee8d2d8bcd94e11a824d92ce5a793a63d7972
Vyankatesh Kshatriya <vyankatesh.kshatriya@gmail.com>
2023-11-23 17:12:40 +0530

    update

--- lib/searchscreen/search_screen.dart
+++ lib/searchscreen/search_screen.dart
@@ -14,35 +14,38 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppbar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          height: 70,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.circular(40),
-            ),
-          ),
-          child: TextField(
-            controller: search,
-            decoration: InputDecoration(
-              hintText: 'Search',
-              prefixIcon: const Icon(Icons.search_rounded),
-              suffixIcon: IconButton(
-                onPressed: () => setState(() {
-                  search.clear();
-                }),
-                icon: const Icon(Icons.clear),
-              ),
-              border: const OutlineInputBorder(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+            child: Container(
+              height: 70,
+              decoration: const BoxDecoration(
                 borderRadius: BorderRadius.all(
                   Radius.circular(40),
                 ),
               ),
+              child: TextField(
+                controller: search,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: const Icon(Icons.search_rounded),
+                  suffixIcon: IconButton(
+                    onPressed: () => setState(() {
+                      search.clear();
+                    }),
+                    icon: const Icon(Icons.clear),
+                  ),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(40),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
